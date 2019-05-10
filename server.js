c9ad7c167a2e8996f384f0f504e737cd82f4f18a
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-10 12:18:26 +0200

    Add fetch post

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const setupServer = require('./setup-server')
-const app = setupServer()
+const setupServer = require('./setup-server');
+const app = setupServer();
 
-const Card = require('./models/Card')
+const Card = require('./models/Card');
 
 app.get('/cards', (req, res) => {
   Card.find()
-    .then(cards => res.json(cards))
-    .catch(err => res.json(err))
-})
+    .then((cards) => res.json(cards))
+    .catch((err) => res.json(err));
+});
+
+app.post('/cards', (req, res) => {
+  Card.create(req.body)
+    .then((card) => res.status(201).json(card))
+    .catch((err) => res.status(500).json(err));
+  console.log('Successful User Update');
+});
