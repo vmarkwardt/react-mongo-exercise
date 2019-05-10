84e6d800b09119df97cc163ee98a3d046bb10ad5
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-10 22:48:55 +0200

    Change tags form list to tag chips

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function Tag({ tagList }) {
-  return <p>Tags -> {tagList.join(', ')}</p>;
+  //return <p className="tag">Tags -> {tagList.join(', ')}</p>;
+  return (
+    <p>
+      {tagList.map((tag) => (
+        <span className="tag">{tag}</span>
+      ))}
+    </p>
+  );
 }
