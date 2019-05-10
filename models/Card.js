a3ff52dd6e6ec49e222a8d96d3624c7d841677d0
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-10 15:02:34 +0200

    Change Card-Model: Add description, tags

--- models/Card.js
+++ models/Card.js
@@ -1,11 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const cardSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-    get: value => value.toUpperCase(),
+    get: (value) => value.toUpperCase(),
   },
-})
+  description: {
+    type: String,
+  },
+  tags: {
+    type: [String],
+  },
+});
 
-module.exports = mongoose.model('Card', cardSchema)
+module.exports = mongoose.model('Card', cardSchema);
+
+//description and tags (an Array of Strings)
