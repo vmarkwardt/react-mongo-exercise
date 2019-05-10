4086bd89fa3791696f555418e05a1f0f7264d885
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-10 18:27:41 +0200

    Add postCards

--- src/services.js
+++ src/services.js
@@ -1,3 +1,26 @@
 export function getCards() {
   return fetch('/cards').then((res) => res.json());
 }
+
+export function postCards(card) {
+  //
+  console.log('services.js -> postCards: ', card);
+
+  return fetch('/cards', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(card),
+  });
+}
+
+export function setLocal(name, data) {
+  localStorage.setItem(name, JSON.stringify(data));
+}
+
+export function getLocal(name) {
+  try {
+    return JSON.parse(localStorage.getItem(name));
+  } catch (error) {
+    console.log(error);
+  }
+}
