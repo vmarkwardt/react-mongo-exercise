16c030d8ecdc6622b57408d240d31d2c8072489f
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-11 00:15:19 +0200

    Add buttons Delete , Edit to Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import Tag from './Tag';
 
-export default function Card({ card, bookmarkOnClick }) {
+export default function Card({ card, bookmarkOnClick, editOnClick }) {
   return (
-    <li>
+    <li className="card">
       <header>
         <h3>{card.title}</h3>
+      </header>
+      <p>{card.description}</p>
+      <Tag tagList={card.tags} />
+      <nav className="card-nav">
+        <div>
+          <button
+            disabled={true}
+            className="card-button button-edit-card"
+            onClick={editOnClick}
+          >
+            Edit
+          </button>
+          <button
+            className="card-button button-delete-card"
+            onClick={editOnClick}
+          >
+            Delete
+          </button>
+        </div>
         <button
-          className={card.isBookmarked ? 'bookmarked' : ''}
+          className={
+            card.isBookmarked
+              ? 'card-button card-button-bookmark bookmarked'
+              : 'card-button card-button-bookmark'
+          }
           onClick={() =>
             bookmarkOnClick({ ...card, isBookmarked: !card.isBookmarked })
           }
         >
-          {card.isBookmarked ? 'bookmarked' : 'set bookmark'}
+          {card.isBookmarked ? 'bookmarked' : 'bookmark'}
         </button>
-      </header>
-      <p>{card.description}</p>
-      <Tag tagList={card.tags} />
+      </nav>
     </li>
   );
 }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Card from './Card';
 
-export default function CardList({ cardList, bookmarkOnClick }) {
+export default function CardList({ cardList, bookmarkOnClick, editOnClick }) {
   return (
     <ul>
       {cardList.map((card) => (
-        <Card card={card} key={card._id} bookmarkOnClick={bookmarkOnClick} />
+        <Card
+          card={card}
+          key={card._id}
+          bookmarkOnClick={bookmarkOnClick}
+          editOnClick={editOnClick}
+        />
       ))}
     </ul>
   );
