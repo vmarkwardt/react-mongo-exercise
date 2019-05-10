7ed369855168fad4bfb3757623667e43c8576c17
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-10 12:19:54 +0200

    Extract getCards into services.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { getCards } from './services';
 
 export default class App extends Component {
   state = {
     cards: [],
-  }
+  };
 
   componentDidMount() {
-    fetch('/cards')
-      .then(res => res.json())
-      .then(data => this.setState({ cards: data }))
-      .catch(error => console.log(error))
+    getCards()
+      .then((data) => this.setState({ cards: data }))
+      .catch((error) => console.log(error));
   }
 
   render() {
-    const { cards } = this.state
+    const { cards } = this.state;
 
     return (
       <main>
         <h1>Cards</h1>
         <ul>
-          {cards.map(card => (
+          {cards.map((card) => (
             <li key={card._id}>{card.title}</li>
           ))}
         </ul>
       </main>
-    )
+    );
   }
 }
